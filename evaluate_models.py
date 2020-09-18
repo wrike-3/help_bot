32006075244c0d4ed85393ff6879cc2c18ab2d57
uteuov <uteuov@segmento.ru>
2020-09-18 03:09:04 +0300

    fix time format

--- evaluate_models.py
+++ evaluate_models.py
@@ -36,10 +36,18 @@ for model_name in ModelNames:
         model=model_name.value,
         acc_top_1=score_top_1 / n,
         acc_top_3=score_top_3 / n,
-        elapsed=np.mean(elapsed_list)
+        elapsed=str(np.mean(elapsed_list)).replace("0 days ", "")
     )
     lines.append(line)
     # print(model_name.value, score_top_1 / n, score_top_3 / n, np.mean(elapsed_list))
 
 report = pd.DataFrame(lines)
+print('from_wrike_org scores')
 print(report)
+
+# from_wrike_org scores
+#      model  acc_top_1  acc_top_3                   elapsed
+# 0  elastic   0.142857   0.220238 0 days 00:00:00.008382589
+# 1     bert   0.035714   0.053571 0 days 00:00:00.125237083
+# 2      bpe   0.059524   0.101190 0 days 00:00:00.024513452
+# 3    tfidf   0.125000   0.125000 0 days 00:00:00.201081690
